927e128827bcfcc1a03de92754aa791f14774c6c
Alcides Figueroa <alcides.f.figueroa@gmail.com>
2018-09-22 09:41:17 -0300

    inicial

---
+++ src/main/java/com/edureka/ms/training/microservice/mstrainingjourny/MstrainingjournyApplication.java
@@ -0,0 +1,12 @@
+package com.edureka.ms.training.microservice.mstrainingjourny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstrainingjournyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstrainingjournyApplication.class, args);
+	}
+}
